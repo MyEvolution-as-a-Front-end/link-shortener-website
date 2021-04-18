024f11559adf0907bac3eb6eefa542b871a42a48
João Alves <njoao97710@gmail.com>
2021-04-18 01:37:52 +0000

    :hammer: REFACTOR(Home): remove useEffect and state isShortener and add func handlesubmit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useRef, useState} from 'react'
+import {useRef, useState} from 'react'
 import {IoCopyOutline} from 'react-icons/io5'
 
 import {api} from 'services/api'
@@ -7,8 +7,6 @@ import styles from 'styles/Home.module.css'
 export default function Home(): JSX.Element {
   const [text, setText] = useState('')
 
-  const [isShortener, setIsShortener] = useState(false)
-
   const inputRef = useRef<HTMLInputElement | null>()
 
   function clipboard(element: HTMLInputElement) {
@@ -16,44 +14,39 @@ export default function Home(): JSX.Element {
     document.execCommand('copy')
   }
 
-  useEffect(() => {
-    async function createShortenerLink() {
-      try {
-        const response = await api.post('shorten', {
-          long_url: text,
-        })
-        setText(response.data?.link)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    if (text && isShortener) {
-      createShortenerLink()
-    }
-  }, [isShortener, text])
+  async function handleSubmit() {
+    const {data} = await api.post('shorten', {
+      long_url: text,
+    })
+    setText(data?.link)
+  }
 
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>
-        Crie links atrativos com apenas um clique
+        Crie links atrativos <br /> com um clique
       </h2>
       <p>Crie links curtos poderosos e reconhecíveis</p>
       <div className={styles.box}>
-        <input
-          ref={inputRef}
-          type="text"
-          value={text}
-          onChange={e => setText(e.target.value)}
-        />
-        {isShortener && (
-          <IoCopyOutline
-            className={styles.icon}
-            onClick={() => clipboard(inputRef.current)}
-            size={16}
+        <div className={styles.input}>
+          <input
+            ref={inputRef}
+            type="text"
+            value={text}
+            onChange={e => setText(e.target.value)}
           />
-        )}
+          {text && (
+            <IoCopyOutline
+              className={styles.icon}
+              onClick={() => clipboard(inputRef.current)}
+              size={18}
+            />
+          )}
+        </div>
+        <button className={styles.button} onClick={() => handleSubmit()}>
+          Encurtar
+        </button>
       </div>
-      <button onClick={() => setIsShortener(true)}>Encurtar</button>
     </div>
   )
 }
